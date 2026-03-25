bdfa3d9ebf8489f979b62201deb5ff95e8016080
douaa-cyber <129712465+douaa-cyber@users.noreply.github.com>
2026-03-25 22:07:09 +0100

    feat: add API routes for users, services, appointments, and customers

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,17 @@ import express from "express";
 //import routes from "./routes";
 import { errorHandler } from "./common/middleware/error.middleware";
 import { notFound } from "./common/middleware/notFound.middleware";
+import userRoutes from "./modules/users/user.routes";
+import serviceRoutes from "./modules/services/service.routes";
+import appointmentRoutes from "./modules/appointments/appointment.routes";
+import customerRoutes from "./modules/customers/customer.routes";
 const app = express();
 app.use(express.json());
 
-//app.use("/api", routes);
+app.use("/api/users", userRoutes);
+app.use("/api/services", serviceRoutes);
+app.use("/api/appointments", appointmentRoutes);
+app.use("/api/customers", customerRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
